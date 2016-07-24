27dc8c219f8929c7deaa142a5342e7b1897c50b4
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2016-07-24 13:07:36 +0300

    Add barebones for the project

---
+++ model/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from settings import DATABASE_URL
+
+# ORM base class
+Base = declarative_base()
+
+
+class Database(object):
+    def __init__(self):
+        self.session = None
+
+    def bind(self, engine):
+        if not self.session:
+            self.session = sessionmaker(bind=engine)(autocommit=True)
+
+
+if __name__ == "__main__":
+    db = Database()
+    engine = create_engine(DATABASE_URL, echo=False)
+    Base.metadata.bind = engine
+    Base.metadata.create_all()
+    db.bind(engine)
---
+++ settings.py
@@ -0,0 +1,1 @@
+DATABASE_URL = 'postgresql://new_habit_bot:123456@localhost:5432/progressbot'
