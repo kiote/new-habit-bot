012055e820d9d26f0de161faf17abc65e05d2a16
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2016-07-25 23:26:20 +0300

    Add models

--- model/entities.py
+++ model/entities.py
@@ -1,4 +1,37 @@
+from functools import partial
+
+from sqlalchemy import Column, ForeignKey,\
+        String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+
 
 # ORM base class
 Base = declarative_base()
+
+
+# Aliases
+PrimaryKey = partial(Column, primary_key=True)
+Required = partial(Column, nullable=False)
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = PrimaryKey(String)
+    name = Column(String)
+
+
+class Habit(Base):
+    __tablename__ = 'habits'
+
+    id = PrimaryKey(String)
+    name = Column(String)
+
+
+class DailyLog(Base):
+    __tablename__ = 'daily_logs'
+
+    id = PrimaryKey(String)
+    date = Required(DateTime)
+    habit_id = Required(ForeignKey(Habit.id))
+    user_id = Required(ForeignKey(User.id))
